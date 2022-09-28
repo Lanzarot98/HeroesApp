315df67220f8f0ef8718ca10dae185beb1bb0405
lanzarot <luis-389@hotmail.com>
2022-09-28 17:41:57 -0500

    implementation: useMemo

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 
@@ -6,7 +7,15 @@ export const HeroPage = () => {
 
   const { id } = useParams();
   
-  const hero = getHeroById( id );
+  const hero = useMemo(() => getHeroById( id ), [ id ]);
+  const navigate = useNavigate();
+
+
+  const onNavigateBack = () => {
+    if (hero.publisher == 'Marvel Comics') return navigate('/marvel');
+    return navigate('/dc');
+    // ó navigate(-1);
+  };
 
   if ( !hero) {
     return (
@@ -19,11 +28,34 @@ export const HeroPage = () => {
     )
   }
 
-  console.log(hero);
-
   return (
-    <>
-      <h1>{hero.superhero}</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={`/assets/heroes/${ id }.jpg` }
+          alt={ hero.superhero } 
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b>{hero.alter_ego}</li>
+          <li className="list-group-item"><b>Publisher: </b>{hero.publisher}</li>
+          <li className="list-group-item"><b>First appearance: </b>{hero.first_appearance}</li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{ hero.characters }</p>
+
+        <button 
+          className="btn btn-outline-primary"
+          onClick={ onNavigateBack }
+        >
+          Back
+        </button>
+
+      </div>
+    </div>
   )
 }
