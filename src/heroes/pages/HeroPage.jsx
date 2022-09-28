cf7410f1897773f906fd717c1caa4140a20dffc0
lanzarot <luis-389@hotmail.com>
2022-09-28 16:02:12 -0500

    section: read arguments by URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,29 @@
+import { useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+  
+  const hero = getHeroById( id );
+
+  if ( !hero) {
+    return (
+      <div className="container">
+        <h1 className="alert alert-info">
+        🦸‍♀️ 404 - Hero not found 🦸‍♂️</h1>
+      </div>
+      // ó puedo retornar esto:
+      // <Navigate to="/marvel" hay que importar el navigate/>
+    )
+  }
+
+  console.log(hero);
+
   return (
-    <h1>heroPage</h1>
+    <>
+      <h1>{hero.superhero}</h1>
+    </>
   )
 }
